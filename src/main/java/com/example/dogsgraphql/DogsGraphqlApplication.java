a96f6764bc0ecd970000345ee8e9282c2bd8ae51
Kennedy Kariuki <kennedy.kariuki@tracom.co.ke>
2019-08-03 19:26:47 +0300

    graphql properties setup

---
+++ src/main/java/com/example/dogsgraphql/DogsGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogsgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogsGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dogsgraphql/DogsGraphqlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.dogsgraphql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DogsGraphqlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
